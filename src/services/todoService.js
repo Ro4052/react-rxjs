36228232116e8f22104c59fcabce4d54535c6a2c
Ro4052 <Ro4052@users.noreply.github.com>
2019-02-19 09:34:28 +0000

    Todo list is now backed by local storage

--- src/services/todoService.js
+++ src/services/todoService.js
@@ -1,17 +1,27 @@
 import { BehaviorSubject } from "rxjs";
 
-let nextId = 0;
-const todos$ = new BehaviorSubject({ todos: [] });
+let nextId = localStorage.getItem("nextId") || 0;
+const todos$ = new BehaviorSubject({
+  todos: JSON.parse(localStorage.getItem("todos")) || []
+});
 
 export const getTodoStream = () => todos$;
 
-export const onSubmitTodo = todoText =>
+export const onSubmitTodo = todoText => {
   todos$.next({
     todos: [...todos$.value.todos, { id: nextId++, text: todoText }]
   });
+  updateLocalStorage();
+};
 
 export const onDeleteTodo = todoId => {
   todos$.next({
     todos: todos$.value.todos.filter(todo => todo.id !== todoId)
   });
+  updateLocalStorage();
 };
+
+function updateLocalStorage() {
+  localStorage.setItem("nextId", nextId);
+  localStorage.setItem("todos", JSON.stringify(todos$.value.todos));
+}
