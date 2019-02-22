3a81e80b8a38cd9f748fc14a88a2e78cef8c2e4b
Ro4052 <Ro4052@users.noreply.github.com>
2019-02-22 09:53:56 +0000

    Wrapped forwardRef in a memo

--- src/components/todoList/todoItemDisp/TodoItemDisp.jsx
+++ src/components/todoList/todoItemDisp/TodoItemDisp.jsx
@@ -1,52 +1,57 @@
-import React, { forwardRef, lazy, Suspense } from "react";
+import React, { memo, forwardRef, lazy, Suspense } from "react";
 import { List, Icon, Loader } from "semantic-ui-react";
 import cx from "classnames";
 
 import styles from "./TodoItemDisp.module.css";
 const TextInput = lazy(() => import("../../textInput/TextInput"));
 
-export default forwardRef((props, textInput) => {
-  const content = props.editMode ? (
-    <div ref={textInput}>
-      <Suspense fallback={<Loader />}>
-        <TextInput
-          placeholder="Type here..."
-          initialText={props.todo.text}
-          submit={props.onSubmit}
-        />
-      </Suspense>
-    </div>
-  ) : (
-    <>
-      <span className={styles.todoSpan} onClick={() => props.setEditMode(true)}>
-        {props.todo.text}
-      </span>
-      <div className={styles.actionIcons}>
-        <Icon
-          link
-          name={props.todo.complete ? "undo alternate" : "check circle"}
-          color={props.todo.complete ? "red" : "green"}
-          onClick={() => props.toggleComplete(props.todo.id)}
-        />
-        <Icon
-          link
-          name="close"
-          color="red"
-          onClick={() => props.delete(props.todo.id)}
-        />
+export default memo(
+  forwardRef((props, textInput) => {
+    const content = props.editMode ? (
+      <div ref={textInput}>
+        <Suspense fallback={<Loader />}>
+          <TextInput
+            placeholder="Type here..."
+            initialText={props.todo.text}
+            submit={props.onSubmit}
+          />
+        </Suspense>
       </div>
-    </>
-  );
-  return (
-    <List.Item
-      className={cx({ [styles.completedTodo]: props.todo.complete })}
-      icon={
-        <Icon
-          name="sticky note outline"
-          color={props.todo.complete ? "green" : "black"}
-        />
-      }
-      content={content}
-    />
-  );
-});
+    ) : (
+      <>
+        <span
+          className={styles.todoSpan}
+          onClick={() => props.setEditMode(true)}
+        >
+          {props.todo.text}
+        </span>
+        <div className={styles.actionIcons}>
+          <Icon
+            link
+            name={props.todo.complete ? "undo alternate" : "check circle"}
+            color={props.todo.complete ? "red" : "green"}
+            onClick={() => props.toggleComplete(props.todo.id)}
+          />
+          <Icon
+            link
+            name="close"
+            color="red"
+            onClick={() => props.delete(props.todo.id)}
+          />
+        </div>
+      </>
+    );
+    return (
+      <List.Item
+        className={cx({ [styles.completedTodo]: props.todo.complete })}
+        icon={
+          <Icon
+            name="sticky note outline"
+            color={props.todo.complete ? "green" : "black"}
+          />
+        }
+        content={content}
+      />
+    );
+  })
+);
