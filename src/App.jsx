fcaa22c7e8d73210fdab7a6635398b22502648cd
Ro4052 <Ro4052@users.noreply.github.com>
2019-02-25 16:44:49 +0000

    Updated app styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 import TodoList from "./components/todoList/TodoList";
+import styles from "./App.module.css";
 
 export default () => {
   return (
-    <>
+    <div className={styles.containerDiv}>
       <h1> React and RxJS Todo List </h1>
       <TodoList />
-    </>
+    </div>
   );
 };
--- src/components/textInput/TextInput.jsx
+++ src/components/textInput/TextInput.jsx
@@ -22,6 +22,7 @@ export default props => {
   return (
     <form onSubmit={handleSubmit}>
       <Input
+        fluid
         action={props.action}
         placeholder={props.placeholder}
         value={inputText}
