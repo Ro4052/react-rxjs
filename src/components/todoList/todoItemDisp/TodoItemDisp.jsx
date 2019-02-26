9991f331bda5d494f766311d605d55b84aa7cd34
Ro4052 <Ro4052@users.noreply.github.com>
2019-02-26 08:26:22 +0000

    Can only edit incomplete todos

--- src/components/todoList/todoItemDisp/TodoItemDisp.jsx
+++ src/components/todoList/todoItemDisp/TodoItemDisp.jsx
@@ -7,42 +7,47 @@ const TextInput = lazy(() => import("../../textInput/TextInput"));
 
 export default memo(
   forwardRef((props, textInput) => {
-    const content = props.editMode ? (
-      <div ref={textInput}>
-        <Suspense fallback={<Loader />}>
-          <TextInput
-            placeholder="Type here..."
-            initialText={props.todo.get("text")}
-            submit={props.onSubmit}
-          />
-        </Suspense>
-      </div>
-    ) : (
-      <>
-        <span
-          className={styles.todoSpan}
-          onClick={() => props.setEditMode(true)}
-        >
-          {props.todo.get("text")}
-        </span>
-        <div className={styles.actionIcons}>
-          <Icon
-            link
-            name={
-              props.todo.get("complete") ? "undo alternate" : "check circle"
-            }
-            color={props.todo.get("complete") ? "red" : "green"}
-            onClick={() => props.toggleComplete(props.todo.get("id"))}
-          />
-          <Icon
-            link
-            name="close"
-            color="red"
-            onClick={() => props.delete(props.todo.get("id"))}
-          />
+    const content =
+      props.editMode && !props.todo.get("complete") ? (
+        <div ref={textInput}>
+          <Suspense fallback={<Loader />}>
+            <TextInput
+              placeholder="Type here..."
+              initialText={props.todo.get("text")}
+              submit={props.onSubmit}
+            />
+          </Suspense>
         </div>
-      </>
-    );
+      ) : (
+        <>
+          <span
+            className={cx({
+              [styles.todoSpan]: !props.todo.get("complete")
+            })}
+            onClick={() =>
+              !props.todo.get("complete") && props.setEditMode(true)
+            }
+          >
+            {props.todo.get("text")}
+          </span>
+          <div className={styles.actionIcons}>
+            <Icon
+              link
+              name={
+                props.todo.get("complete") ? "undo alternate" : "check circle"
+              }
+              color={props.todo.get("complete") ? "red" : "green"}
+              onClick={() => props.toggleComplete(props.todo.get("id"))}
+            />
+            <Icon
+              link
+              name="close"
+              color="red"
+              onClick={() => props.delete(props.todo.get("id"))}
+            />
+          </div>
+        </>
+      );
     return (
       <List.Item
         className={cx({ [styles.completedTodo]: props.todo.get("complete") })}
