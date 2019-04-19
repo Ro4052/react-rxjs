990e6f7fcf56c1f85561cc412fd5862f25457ed3
Ro4052 <Ro4052@users.noreply.github.com>
2019-04-19 10:21:55 +0100

    Fix empty local storage on startup case

--- src/services/todoService/todoLocalStorage.js
+++ src/services/todoService/todoLocalStorage.js
@@ -3,7 +3,7 @@ import { fromJS } from "immutable";
 export const getNextId = () => localStorage.getItem("nextId") || 0;
 
 export const getStoredTodos = () =>
-  fromJS(JSON.parse(localStorage.getItem("todos") || []));
+  fromJS(JSON.parse(localStorage.getItem("todos")) || []);
 
 export const updateLocalStorage = (nextId, todos) => {
   localStorage.setItem("nextId", nextId);
